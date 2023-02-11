ae76740112c1014d04be5e089e9f7407048d8f6d
amit369 <amitchaman369@gmail.com>
2023-02-11 19:47:20 +0530

    changes done

--- App.js
+++ App.js
@@ -4,19 +4,38 @@ import {
   StyleSheet,
   Text,
   View,
+  Button
 } from 'react-native';
 
-
+function fruit () 
+{
+  return "apple";
+}
 const App = () => {
-
+  const name = "Amit Chaman";
+  const fruit = (val) => {
+    console.warn(val)
+  }
   return (
           <View>
-            <Text>Hello World </Text>
+            <Text style={{ fontSize : 50}}> {fruit()} </Text>
+            <UserData />
+             <Button title="On Press" onPress= {fruit} color={'green'} />
+             <Button title="On Press" onPress= { () => { fruit("Hello") }} color={'green'} />
+
         </View>
-     
   );
 };
 
+const UserData = () => {
+  return(
+    <View>
+       <Text style={{ fontSize : 20}} >Name : Anil </Text>
+       <Text style={{ fontSize : 20}} >Age : 29 </Text>
+       <Text style={{ fontSize : 20}} >Email : anil@test.com </Text>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
