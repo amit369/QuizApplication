ed8805e4a8d0ab32f660690494e8430fbdb4227b
amit369 <amitchaman369@gmail.com>
2023-02-12 16:30:26 +0530

    changes done

--- App.js
+++ App.js
@@ -1,57 +1,63 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button
-} from 'react-native';
-
-function fruit () 
-{
-  return "apple";
-}
 const App = () => {
-  const name = "Amit Chaman";
-  const fruit = (val) => {
-    console.warn(val)
+  const [name, setName] = useState('Anil');
+  const [email, setEmail] = useState('email');
+  const [password, setPassword] = useState('password');
+  const [display, setDisplay] = useState();
+  const resetFormData = () => {
+    setDisplay(false);
+    setEmail("");
+    setPassword("");
+    setName("");
   }
-  return (
-          <View>
-            <Text style={{ fontSize : 50}}> {fruit()} </Text>
-            <UserData />
-             <Button title="On Press" onPress= {fruit} color={'green'} />
-             <Button title="On Press" onPress= { () => { fruit("Hello") }} color={'green'} />
+  return <View>
+        <Text style={{fontSize : 30 }}>Simple Form in React Native </Text>
+        <TextInput placeholder="Enter your name "  style={styles.textInput} onChangeText= { (text) => {
+               setName(text)
+        }}  value={name} />
+        <TextInput placeholder="Enter your password  "  style={styles.textInput} onChangeText= { (text) => {
+         setPassword(text)}} secureTextEntry={true} value={password}  />
 
-        </View>
-  );
+         <TextInput placeholder="Enter user email "  style={styles.textInput} onChangeText= { (text) => {
+          setEmail(text)}} value={email} />
+          <View style={{ marginBottom : 10}} >
+          <Button title="Print Value"  style={{ margin: 10 }} onPress={ () => { setDisplay(true) }}/>
+          {
+            display ? <View>       
+              <Text style={{ fontSize : 15}} >User Name is : {name} </Text>
+              <Text style={{ fontSize : 15}} >User Email is : {email} </Text>
+              <Text style={{ fontSize : 15}} >User Password is : {password} </Text>
+              </View> : null
+          }
+          </View>
+          <Button title="Clear Details" color = {"green"} onPress= { () => { 
+            resetFormData()
+          }} />
+  </View>;
 };
 
-const UserData = () => {
-  return(
-    <View>
-       <Text style={{ fontSize : 20}} >Name : Anil </Text>
-       <Text style={{ fontSize : 20}} >Age : 29 </Text>
-       <Text style={{ fontSize : 20}} >Email : anil@test.com </Text>
-    </View>
-  )
-}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  textBox: {
+    color: 'blue',
+    fontSize: 25,
+    backgroundColor: 'pink',
+    marginBottom: 10,
+    padding: 4,
+    borderRadius: 10,
+    height: 100,
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    borderColor: 'red',
+    borderWidth: 2,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  textInput: {
+    fontSize: 10,
+    color: 'red',
+    borderWidth: 2,
+    borderColor: 'red',
+    margin: 10,
   },
 });
 
